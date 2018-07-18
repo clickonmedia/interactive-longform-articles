c70a8450d8124905f1f85de08d272c8026883182
Adrian Goris <adriangoris@gmail.com>
2018-07-18 14:12:20 +0100

    migrating over to cmb2 from acf

---
+++ goodtobebad-template.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Template Name: Good to Be Bad
+ * Description: A Page Template with a darker design.
+ */
+
+echo '<iframe width="1280" height="720" src="//www.youtube.com/embed/2Bls1KKDwmo" frameborder="0" allowfullscreen></iframe>';
