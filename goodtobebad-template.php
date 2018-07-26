01640276cebec45a2e3da38697d022e65c3a731e
jpylisela <jpylisela@gmail.com>
2018-07-26 10:42:20 +0100

    move cmb2 to v2

--- goodtobebad-template.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- * Template Name: Good to Be Bad
- * Description: A Page Template with a darker design.
- */
-
-echo '<iframe width="1280" height="720" src="//www.youtube.com/embed/2Bls1KKDwmo" frameborder="0" allowfullscreen></iframe>';
